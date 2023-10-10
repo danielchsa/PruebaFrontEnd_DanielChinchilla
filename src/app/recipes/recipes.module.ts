d9e83d86f444351d36d3a60d91faeffa9980345b
danielchsa <dan722sz@gmail.com>
2023-10-09 18:54:02 -0600

    FormsModule Import

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -4,11 +4,11 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { DetailComponent } from './pages/detail/detail.component';
 import { AddComponent } from './pages/add/add.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ComponentsModule } from '../components/components.module';
 
 @NgModule({
   declarations: [DashboardComponent, DetailComponent, AddComponent],
-  imports: [CommonModule, ReactiveFormsModule, ComponentsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, ComponentsModule],
 })
 export class RecipesModule {}
